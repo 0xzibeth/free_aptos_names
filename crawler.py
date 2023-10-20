d4c77f3cacdedce84ebc2d92f04788ec46d3021e
0xzibeth <128691289+0xzibeth@users.noreply.github.com>
2023-10-20 17:11:35 +0100

    Update crawler.py logic and comments

--- crawler.py
+++ crawler.py
@@ -4,20 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-words_source = "https://raw.githubusercontent.com/MichaelWehar/Public-Domain-Word-Lists/master/5000-more-common.txt"
+words_source = "https://raw.githubusercontent.com/MichaelWehar/Public-Domain-Word-Lists/master/5000-more-common.txt" # or any other source
 response = requests.get(words_source)
 words = response.text.split()
-driver = webdriver.Safari()
+driver = webdriver.Safari() # or Chrome()
 for word in words:
     try:
-        if len(word) >= 6:
-            url = f'https://www.aptosnames.com/domain-search/{word}'
-            driver.get(url)
-            time.sleep(1)
-            value = driver.find_element(By.CLASS_NAME, 'css-il8357')
+        url = f'https://www.aptosnames.com/domain-search/{word}'
+        driver.get(url)
+        time.sleep(1)
+        value = driver.find_element(By.CLASS_NAME, 'css-il8357') # class `css-il8357` might need an additional check in browser Inspector and update
 
-            if value.text == 'Available':
-                print(word)
+        if value.text == 'Available':
+            print(word)
+            
     except Exception:
         time.sleep(1)
 
